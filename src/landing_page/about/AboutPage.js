9df4d0fcb0a0c4ba19200ab1abbbb47bf00c5b80
Sarvesh <sarveshv0457@gmail.com>
2026-01-26 13:04:15 +0530

    created about page

--- src/landing_page/about/AboutPage.js
+++ src/landing_page/about/AboutPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Hero from './Hero';
+import Team from './Team';
 
 function About() {
     return ( 
         <>
-            <h1>About Page</h1>
+            <Hero />
+            <Team />
         </>
      );
 }
